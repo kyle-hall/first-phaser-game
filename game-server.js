b2117865ce44ce6d45f465baae18e985df38847a
Kyle Hall <hallkd47@gmail.com>
2016-09-02 12:32:14 -0500

    restructures project and removes express server

--- game-server.js
+++
@@ -1,9 +0,0 @@
-
-var express = require('express'),
-    app = express();
-
-app.use(express.static('game'));
-
-app.listen(3000, function () {
-  console.log("Your server is now running on port 3000...");
-});
